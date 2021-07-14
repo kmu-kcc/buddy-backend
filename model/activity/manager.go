1d06af91474f066e60e2b4fb9e3925c54e35911a
Limseunghyun99 <lcj8833@gmail.com>
2021-07-14 20:11:59 +0900

    Add Activity Structure & New() Function

--- model/activity/manager.go
+++ model/activity/manager.go
@@ -1,1 +1,28 @@
 package activity
+
+//activity represents an activity state
+type Activity struct {
+	ID          string   `json:"id"`          //Activity ID*
+	Date        uint     `json:"date"`        //Event Day
+	Participant []string `json:"participant"` //Activity Participants list
+	Place       string   `json:"place"`       //Event Place
+	Summary     string   `json:"summary"`     //Short Summary for event
+	Kind        string   `json:"kind"`        //Event Type
+	Picture     []string `json:"picture"`     //Event Pictures, Path of Picture
+}
+
+//New makes an Activity
+//Making new Activity needs Authentication with e-mail
+func New(id string, date uint, place string, summary string, kind string) (*Activity, error) {
+	//TODO
+	//e-mail Verification (Auth)
+	return &Activity{
+		ID:          id,
+		Date:        date,
+		Participant: []string{},
+		Place:       place,
+		Summary:     summary,
+		Kind:        kind,
+		Picture:     []string{},
+	}, nil
+}
