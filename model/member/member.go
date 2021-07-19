f85255d0d85e1325ae1ccd3af0ebcc58dedbfb1e
maengsanha <msh0117@sogang.ac.kr>
2021-07-20 04:38:04 +0900

    Implement core logics for signin/signups/exits/cancelexit

--- model/member/member.go
+++ model/member/member.go
@@ -13,20 +13,20 @@ import (
 
 // Member represents a club member state.
 type Member struct {
-	ID         string `json:"id"`         // student ID
-	password   string `json:"-"`          // password - soon deprecated
-	Name       string `json:"name"`       // Name
-	Department string `json:"department"` // department - magic number needed
-	Grade      string `json:"grade"`      // grade
-	Phone      string `json:"phone"`      // phone number
-	Email      string `json:"email"`      // e-mail address
-	Enrollment string `json:"enrollment"` // enrollment state (attending/absent/graduated) - magic number needed
-	Verified   bool   `json:"verified"`   // e-mail verified or not
-	Approved   bool   `json:"approved"`   // approved or not
-	Privileged bool   `json:"privileged"` // is administrator or not - soon deprecated
-	OnDelete   bool   `json:"on_delete"`  // on exit process or not
-	CreatedAt  int64  `json:"created_at"` // when created - Unix timestamp
-	UpdatedAt  int64  `json:"updated_at"` // last updated - Unix timestamp
+	ID         string `json:"id" bson:"id"`                 // student ID
+	password   string `json:"-" bson:"-"`                   // password - soon deprecated
+	Name       string `json:"name" bson:"name"`             // Name
+	Department string `json:"department" bson:"department"` // department - magic number needed
+	Grade      string `json:"grade" bson:"grade"`           // grade
+	Phone      string `json:"phone" bson:"phone"`           // phone number
+	Email      string `json:"email" bson:"email"`           // e-mail address
+	Enrollment string `json:"enrollment" bson:"enrollment"` // enrollment state (attending/absent/graduated) - magic number needed
+	Verified   bool   `json:"verified" bson:"verified"`     // e-mail verified or not
+	Approved   bool   `json:"approved" bson:"approved"`     // approved or not
+	Privileged bool   `json:"privileged" bson:"privileged"` // is administrator or not - soon deprecated
+	OnDelete   bool   `json:"on_delete" bson:"on_delete"`   // on exit process or not
+	CreatedAt  int64  `json:"created_at" bson:"created_at"` // when created - Unix timestamp
+	UpdatedAt  int64  `json:"updated_at" bson:"updated_at"` // last updated - Unix timestamp
 }
 
 // New returns a new club member.
@@ -49,23 +49,41 @@ func New(id string, name string, department string, grade string, phone string, 
 	}
 }
 
-// toBSON converts m to an ordered BSON document.
-func (m Member) toBSON() bson.D {
-	return bson.D{
-		bson.E{Key: "id", Value: m.ID},
-		bson.E{Key: "password", Value: m.password},
-		bson.E{Key: "name", Value: m.Name},
-		bson.E{Key: "department", Value: m.Department},
-		bson.E{Key: "grade", Value: m.Grade},
-		bson.E{Key: "phone", Value: m.Phone},
-		bson.E{Key: "email", Value: m.Email},
-		bson.E{Key: "enrollment", Value: m.Enrollment},
-		bson.E{Key: "verified", Value: m.Verified},
-		bson.E{Key: "approved", Value: m.Approved},
-		bson.E{Key: "privileged", Value: m.Privileged},
-		bson.E{Key: "on_delete", Value: m.OnDelete},
-		bson.E{Key: "created_at", Value: m.CreatedAt},
-		bson.E{Key: "updated_at", Value: m.UpdatedAt},
+// SingIn checks whether m is verified and approved.
+//
+// NOTE:
+//
+// It is a member-limited operation:
+//	Only the authenticated members can access to this operation.
+func (m Member) SingIn() error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	// TODO
+	//
+	// update URI
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		return err
+	}
+
+	member := new(Member)
+
+	if err = client.Database("club").Collection("members").FindOne(ctx, bson.D{bson.E{Key: "id", Value: m.ID}}).Decode(member); err == mongo.ErrNoDocuments {
+		if err = client.Disconnect(ctx); err != nil {
+			return err
+		}
+		return mongo.ErrNoDocuments
+	} else if err != nil {
+		return err
+	} else if err = client.Disconnect(ctx); err != nil {
+		return err
+	} else if !member.Verified {
+		return errors.New("not yet verified")
+	} else if !member.Approved {
+		return errors.New("under review")
+	} else {
+		return nil
 	}
 }
 
@@ -77,6 +95,7 @@ func (m Member) SignUp() error {
 	defer cancel()
 
 	// TODO
+	//
 	// update URI
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
@@ -86,8 +105,11 @@ func (m Member) SignUp() error {
 	collection := client.Database("club").Collection("members")
 	member := new(Member)
 
-	if err = collection.FindOne(ctx, bson.D{bson.E{Key: "id", Value: m.ID}}).Decode(member); err == mongo.ErrNoDocuments {
-		if _, err = collection.InsertOne(ctx, m.toBSON()); err != nil {
+	if err = collection.FindOne(
+		ctx,
+		bson.D{bson.E{Key: "id", Value: m.ID}}).
+		Decode(member); err == mongo.ErrNoDocuments {
+		if _, err = collection.InsertOne(ctx, m); err != nil {
 			return err
 		}
 		return client.Disconnect(ctx)
@@ -102,6 +124,50 @@ func (m Member) SignUp() error {
 	}
 }
 
+// SignUps returns the signup request list.
+//
+// NOTE:
+//
+// It is a privileged operation:
+//	Only the club managers can access to this operation.
+func SignUps() (members []Member, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	// TODO
+	//
+	// update URI
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		return
+	}
+
+	cur, err := client.Database("club").
+		Collection("members").
+		Find(
+			ctx,
+			bson.D{bson.E{Key: "approved", Value: false}})
+
+	if err != nil {
+		return
+	}
+
+	member := new(Member)
+
+	for cur.Next(ctx) {
+		if err = cur.Decode(member); err != nil {
+			return
+		}
+		members = append(members, *member)
+	}
+
+	if err = cur.Close(ctx); err != nil {
+		return
+	}
+
+	return members, client.Disconnect(ctx)
+}
+
 // Approve approves the signup requests of ids.
 //
 // NOTE:
@@ -113,6 +179,7 @@ func Approve(ids []string) error {
 	defer cancel()
 
 	// TODO
+	//
 	// update URI
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
@@ -121,8 +188,17 @@ func Approve(ids []string) error {
 
 	collection := client.Database("club").Collection("members")
 
+	// FIXME
+	//
+	// This loop takes O(N) disk access and O(N^2) lookups in the worst case.
+	// Consider using $in and bson.A
+	//
+	// See https://www.mongodb.com/blog/post/mongodb-go-driver-tutorial
 	for _, id := range ids {
-		if _, err = collection.UpdateOne(ctx, bson.D{bson.E{Key: "id", Value: id}}, bson.D{bson.E{Key: "approved", Value: true}}); err != nil {
+		if _, err = collection.UpdateOne(
+			ctx,
+			bson.D{bson.E{Key: "id", Value: id}},
+			bson.D{bson.E{Key: "approved", Value: true}}); err != nil {
 			return err
 		}
 	}
@@ -141,6 +217,7 @@ func Reject(ids []string) error {
 	defer cancel()
 
 	// TODO
+	//
 	// update URI
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
@@ -149,8 +226,16 @@ func Reject(ids []string) error {
 
 	collection := client.Database("club").Collection("members")
 
+	// FIXME
+	//
+	// This loop takes O(N) disk access and O(N^2) lookups in the worst case.
+	// Consider using $in and bson.A
+	//
+	// See https://www.mongodb.com/blog/post/mongodb-go-driver-tutorial
 	for _, id := range ids {
-		if _, err = collection.DeleteOne(ctx, bson.D{bson.E{Key: "id", Value: id}}); err != nil {
+		if _, err = collection.DeleteOne(
+			ctx,
+			bson.D{bson.E{Key: "id", Value: id}}); err != nil {
 			return err
 		}
 	}
@@ -169,6 +254,7 @@ func (m Member) Exit() error {
 	defer cancel()
 
 	// TODO
+	//
 	// update URI
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
@@ -200,6 +286,50 @@ func (m Member) Exit() error {
 	}
 }
 
+// Exits returns the exit request list.
+//
+// NOTE:
+//
+// It is a privileged operation:
+//	Only the club managers can access to this operation.
+func Exits() (members []Member, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	// TODO
+	//
+	// update URI
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		return
+	}
+
+	cur, err := client.Database("club").
+		Collection("members").
+		Find(
+			ctx,
+			bson.D{bson.E{Key: "on_delete", Value: true}})
+
+	if err != nil {
+		return
+	}
+
+	member := new(Member)
+
+	for cur.Next(ctx) {
+		if err = cur.Decode(member); err != nil {
+			return
+		}
+		members = append(members, *member)
+	}
+
+	if err = cur.Close(ctx); err != nil {
+		return
+	}
+
+	return members, client.Disconnect(ctx)
+}
+
 // Delete deletes the members of ids.
 //
 // NOTE:
@@ -211,6 +341,7 @@ func Delete(ids []string) error {
 	defer cancel()
 
 	// TODO
+	//
 	// update URI
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
@@ -220,7 +351,9 @@ func Delete(ids []string) error {
 	collection := client.Database("club").Collection("members")
 
 	for _, id := range ids {
-		if _, err = collection.DeleteOne(ctx, bson.D{bson.E{Key: "id", Value: id}}); err != nil {
+		if _, err = collection.DeleteOne(
+			ctx,
+			bson.D{bson.E{Key: "id", Value: id}}); err != nil {
 			return err
 		}
 	}
@@ -228,7 +361,50 @@ func Delete(ids []string) error {
 	return client.Disconnect(ctx)
 }
 
-// Members returns the all member state.
+// CancelExit cancels the exit request of m.
+//
+// NOTE:
+//
+// It is a member-limited operation:
+//	Only the authenticated members can access to this operation.
+func (m Member) CancelExit() error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	// TODO
+	//
+	// update URI
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		return err
+	}
+
+	member := new(Member)
+
+	if err = client.Database("club").
+		Collection("members").
+		FindOneAndUpdate(
+			ctx,
+			bson.D{bson.E{Key: "id", Value: m.ID}},
+			bson.D{bson.E{Key: "on_delete", Value: false}}).
+		Decode(member); err != nil {
+		return err
+	}
+
+	if member.OnDelete {
+		if err = client.Disconnect(ctx); err != nil {
+			return err
+		}
+		return errors.New("exit cancel request success")
+	} else {
+		if err = client.Disconnect(ctx); err != nil {
+			return err
+		}
+		return errors.New("never applied for exit")
+	}
+}
+
+// Members returns the all club member state.
 //
 // NOTE:
 //
@@ -239,20 +415,24 @@ func Members() (members []Member, err error) {
 	defer cancel()
 
 	// TODO
+	//
 	// update URI
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
 		return
 	}
 
-	member := new(Member)
-
 	cur, err := client.Database("club").
 		Collection("members").
-		Find(ctx, bson.D{})
+		Find(
+			ctx,
+			bson.D{bson.E{Key: "approved", Value: true}})
+
 	if err != nil {
 		return
 	}
+
+	member := new(Member)
 
 	for cur.Next(ctx) {
 		if err = cur.Decode(member); err != nil {
@@ -271,11 +451,13 @@ func Members() (members []Member, err error) {
 // Verify sets m to be verified.
 func (m Member) Verify() error {
 	// TODO
+	//
 	// e-mail verification (OAuth)
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
 	// TODO
+	//
 	// update URI
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
@@ -284,7 +466,8 @@ func (m Member) Verify() error {
 
 	if _, err = client.Database("club").
 		Collection("member").
-		UpdateOne(ctx,
+		UpdateOne(
+			ctx,
 			bson.D{bson.E{Key: "id", Value: m.ID}},
 			bson.D{bson.E{Key: "verified", Value: true}}); err != nil {
 		return err
