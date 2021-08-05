1795c45460bd2bf1e0f9fe07d4057b77a7dc7e31
silver0108 <kk1170@naver.com>
2021-08-06 02:06:15 +0900

    Implement router layer for activity&fee APIs

--- web/api/v1/fee/handlers.go
+++ web/api/v1/fee/handlers.go
@@ -1,2 +1,95 @@
 // Package fee defines the router layer of the club fee of the Buddy System.
 package fee
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kmu-kcc/buddy-backend/pkg/fee"
+)
+
+func Create() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer c.Request.Body.Close()
+
+		body := new(struct {
+			Year     int `json:"year,string"`
+			Semester int `json:"semester,string"`
+			Amount   int `json:"amount,string"`
+		})
+
+		resp := new(struct {
+			Error string `json:"error"`
+		})
+
+		if err := json.NewDecoder(c.Request.Body).Decode(body); err != nil {
+			resp.Error = err.Error()
+			return
+		}
+
+		if err := fee.Create(body.Year, body.Semester, body.Amount); err != nil {
+			resp.Error = err.Error()
+			c.JSON(http.StatusBadRequest, resp)
+			return
+		}
+		c.JSON(http.StatusOK, resp)
+	}
+}
+
+func Submit() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer c.Request.Body.Close()
+
+		body := new(struct {
+			MemberID string `json:"member_id"`
+			Year     int    `json:"year,string"`
+			Semester int    `json:"semester,string"`
+			Amount   int    `json:"amount,string"`
+		})
+
+		resp := new(struct {
+			Error string `json:"error"`
+		})
+
+		if err := json.NewDecoder(c.Request.Body).Decode(body); err != nil {
+			resp.Error = err.Error()
+			return
+		}
+
+		if err := fee.Submit(body.MemberID, body.Year, body.Semester, body.Amount); err != nil {
+			resp.Error = err.Error()
+			c.JSON(http.StatusBadRequest, resp)
+			return
+		}
+		c.JSON(http.StatusOK, resp)
+	}
+}
+
+func Amount() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer c.Request.Body.Close()
+
+		body := new(struct {
+			MemberID string `json:"member_id"`
+			Year     int    `json:"year,string"`
+			Semester int    `json:"semester,string"`
+		})
+
+		resp := new(struct {
+			Error string `json:"error"`
+		})
+
+		if err := json.NewDecoder(c.Request.Body).Decode(body); err != nil {
+			resp.Error = err.Error()
+			return
+		}
+
+		if _, err := fee.Amount(body.Year, body.Semester, body.MemberID); err != nil {
+			resp.Error = err.Error()
+			c.JSON(http.StatusBadRequest, resp)
+			return
+		}
+		c.JSON(http.StatusOK, resp)
+	}
+}
