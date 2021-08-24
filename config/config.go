c70b41de17817e77bf14756df16bf29f730c4187
maengsanha <msh0117@sogang.ac.kr>
2021-08-24 23:54:12 +0900

    Move CORS config to config/

--- config/config.go
+++ config/config.go
@@ -1,8 +1,20 @@
 package config
 
-import "os"
+import (
+	"os"
+	"time"
+
+	"github.com/gin-contrib/cors"
+)
 
 var (
 	MongoURI     = os.Getenv("MONGO_URI")
 	AccessSecret = os.Getenv("ACCESS_SECRET")
+	CORSConfig   = cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"*"},
+		AllowCredentials: true,
+		MaxAge:           6 * time.Hour,
+	}
 )
